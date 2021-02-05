cb62ccf752faaed2759754454b43a80e6045db7a
Henry Ma <haitao.ma@transwarp.io>
2021-02-05 16:02:27 +0800

    init client

---
+++ client/single_client.py
@@ -0,0 +1,17 @@
+#########################################################################
+# File Name: single_client.py
+# Author: Henry Ma
+# mail: iloveicRazavi@gmail.com
+# Create Time: 2021年02月05日 星期五 16时01分13秒
+#########################################################################
+
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+from builtins import *
+
+import cv2
+import numpy as np
+
+
+if __name__ == "__main__":
+
